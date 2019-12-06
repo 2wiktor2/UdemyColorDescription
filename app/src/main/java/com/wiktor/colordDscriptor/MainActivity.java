bc743d548a4c9f75ea4ee51a5d7e81034131bf88
2wiktor2 <wiktor2011@gmail.com>
2019-12-06 08:56:59 +0300

    завершение урока 34-35 раздел 4

--- app/src/main/java/com/wiktor/colordDscriptor/MainActivity.java
+++ app/src/main/java/com/wiktor/colordDscriptor/MainActivity.java
@@ -3,12 +3,36 @@ package com.wiktor.colordDscriptor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Spinner spinner;
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        textView = findViewById(R.id.tv_show_description);
+        button = findViewById(R.id.b_find_description);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int position = spinner.getSelectedItemPosition();
+        textView.setText(getStringFromPosition(position));
+    }
+
+    public String getStringFromPosition(int position) {
+        String[] descriptions = getResources().getStringArray(R.array.descriptionOfTemperament);
+        return descriptions[position];
     }
 }
